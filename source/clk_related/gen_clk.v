88d13387a8206239b1ce5a6c28c5cef731ae1e67
minos <minos@ubuntu>
2011-10-21 14:46:32 -0700

    Goal: gen_clk for neuron_clk, sim_clk, and spindle_clk. Relationship: f200M = f_neuron * Na f_neuron = f_sim * NUM_NEURON * 4 (each Izh-neuron requires 4 clk cycles to I/O) f_sim = SAMPLING_RATE * timefold_Real_Time

---
+++ source/clk_related/gen_clk.v
@@ -0,0 +1,35 @@
+module gen_clk(rawclk, half_cnt, clk_out1, clk_out2, clk_out3);
+    parameter NN = 8; // 2^(NN+1) = NUM_NEURON
+    parameter SR = 10; // 2^SR = SAMPLING_RATE
+    input rawclk;
+    input [31:0] half_cnt;
+    output reg clk_out1, clk_out2, clk_out3;
+
+    reg [31:0] delay_cnt;
+
+    always @ (posedge rawclk) begin
+        if (delay_cnt < half_cnt) begin
+            clk_out1 <= clk_out1;
+            delay_cnt <= delay_cnt + 1;
+        end
+        else begin
+            clk_out1 <= ~clk_out1;
+            delay_cnt <= 0;
+        end
+    end
+
+	reg [NN+2:0] neuronCounter;
+	wire [NN:0] neuronIndex;
+
+	assign neuronIndex = neuronCounter[NN+2:2];
+
+
+	always @ (posedge clk_out1)
+	begin	
+		neuronCounter <= neuronCounter + 1'b1;
+        clk_out2 <= {neuronCounter == 0};
+        clk_out3 <= {(neuronIndex == 0) || (neuronIndex == 9'd85) ||
+                    (neuronIndex == 9'd170)};
+	end
+
+endmodule
