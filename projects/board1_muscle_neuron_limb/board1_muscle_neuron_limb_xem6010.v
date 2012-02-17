11572b68e643b3e595229a0a707ca22410ab9761
eric <eric@localhost>
2012-02-17 12:18:35 -0800

    snapshot

--- projects/board1_muscle_neuron_limb/board1_muscle_neuron_limb_xem6010.v
+++ projects/board1_muscle_neuron_limb/board1_muscle_neuron_limb_xem6010.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
 // Creator: C. Minos Niu
 // 
@@ -212,7 +213,6 @@ module board1_muscle_neuron_limb_xem6010(
 
 
     wire [31:0] master_out;
-    wire rdy;
     wire [31:0] slave_out;
      
     //master sending out     
@@ -223,21 +223,7 @@ module board1_muscle_neuron_limb_xem6010(
     
     //slave receiving in
     //wire XLXN_5;  //mosi
-    wire SSEL_r;   //ssel
-    wire SCK_r;  //sck
-    wire DATA_r; //miso
-   
-//    //more wires for multi transfer
-//    wire XLXN_9;  //mosi
-//    wire XLXN_10;   //ssel
-//    wire XLXN_11;  //sck
-//    wire XLXN_12; //miso
-//    
-//    wire XLXN_13;  //mosi
-//    wire XLXN_14;   //ssel
-//    wire XLXN_15;  //sck
-//    wire XLXN_16; //miso
-    
+
 
     
      wire MISO_data, endmsg;
@@ -255,6 +241,16 @@ module board1_muscle_neuron_limb_xem6010(
 
 //
     //slave module
+    
+    wire rdy;
+    wire SSEL_r;   //ssel
+    wire SCK_r;  //sck
+    wire DATA_r; //miso   
+     //input SPI pins (2)
+    assign SCK_r = pin_jp1_41;  //SCK
+    assign DATA_r = pin_jp1_42;   //MOSI
+    assign SSEL_r = pin_jp1_43;   //SSEL
+
 	wire [31:0] f_rawfr_Ia_spi;
     spi_slave  bodyslave (.clk(clk1), 
                      .en(1'b1), 
@@ -290,10 +286,7 @@ module board1_muscle_neuron_limb_xem6010(
     assign pin_jp1_45 = DATA_s;   //MOSI
     assign pin_jp1_46 = SSEL_s;   //SSEL
         
-     //input SPI pins (2)
-    assign SCK_r = pin_jp1_41;  //SCK
-    assign DATA_r = pin_jp1_42;   //MOSI
-    assign SSEL_r = pin_jp1_43;   //SSEL
+
 	
     //assign XLXN_4 = pin_jp1_50;    //MISO
     //sim_clk 
@@ -304,7 +297,6 @@ module board1_muscle_neuron_limb_xem6010(
 //**  end of spi communication
 
 
-
     //// *** Spindle: f_muscle_len => f_rawfr_Ia
    //wire [31:0] f_rawfr_Ia, x_0, x_1, f_rawfr_II;
 
@@ -313,115 +305,24 @@ module board1_muscle_neuron_limb_xem6010(
     // GET f_rawfr_Ia AND f_rawfr_II FROM BOARD2!!!
 
 
-    // *** Izhikevich: f_fr_Ia_bic => spikes
-        // *** Convert float_fr to int_I1
-	
-    wire [31:0] f_fr_Ia_bic;
-    wire [31:0] i_synI_Ia_bic;
-	mult scale_pps_Ia( .x(f_rawfr_Ia), .y(f_pps_coef_Ia), .out(f_fr_Ia_bic));
-    floor float_to_int_Ia( .in(f_fr_Ia_bic), .out(i_synI_Ia_bic) );
+    //instantiate motor unit 
     
-    wire Ia_spike, s_Ia;
-    wire signed [17:0] v_Ia;   // cell potentials
-    Iz_neuron #(.NN(NN),.DELAY(10)) Ia_neuron
-    (v_Ia,s_Ia, a,b,c,d, i_synI_Ia_bic, neuron_clk, reset_sim, neuronIndex, neuronWriteEnable, readClock, 4'h2, Ia_spike);
-//
-//    wire [31:0] f_fr_II;
-//    wire [31:0] i_synI_II;
-//    mult scale_pps_II( .x(f_rawfr_II), .y(f_pps_coef_II), .out(f_fr_II));
-//    floor float_to_int_II( .in(f_fr_II), .out(i_synI_II) );
-//    wire II_spike, s_II;
-//    wire signed [17:0] v_II;   // cell potentials
-//    Iz_neuron #(.NN(NN),.DELAY(10)) II_neuron
-//    (v_II,s_II, a,b,c,d, i_synI_II, neuron_clk, reset_sim, neuronIndex, neuronWriteEnable, readClock, 4'h2, II_spike);
+    wire    [31:0]  f_total_force;
+    
+    motorunit  #(.NN(NN)) bicep(.f_muscle_length(f_muscle_len), 
+                                //.vel(),
+                                .f_rawfr_Ia(f_rawfr_Ia),
+                                .f_pps_coef_Ia(f_pps_coef_Ia),
+                                .sim_clk(sim_clk),
+                                .neuron_clk(neuron_clk),
+                                .ti_clk(ti_clk),
+                                .reset_sim(reset_sim),
+                                .gain(gain),
+                                .i_gain_MN(i_gain_MN),
+                                .f_total_force(f_total_force)   // output
+                                );
+        
 
-    //*** Synapse:: spike -> I   
-	wire [17:0]  I_out;
-	wire [17:0]	w1, w2, w3;
-	wire spk1, spk2, spk3;
-    wire [31:0] i_postsyn_I;
-  
-	wire signed [17:0] Ia_w1, Ia_w2;  //learned synaptic weights
-
-	synapse   #(.NN(NN)) synIa(I_out, 	Ia_spike, 18'sh01000, 	1'b0, 	18'h0, 			1'b0, 	18'h0, 1'b0, 
-								neuron_clk, reset_sim, neuronIndex, neuronWriteEnable, readClock, 0, 0, Ia_w1, Ia_w2, 
-								0, 0);    
-    
-	assign i_postsyn_I = {14'h0, I_out};
-    
-    // *** izh-Motoneuron :: i_postsyn_I -> (MN_spike, rawspike)
-    
-	wire [3:0] a, b, tau;
-	wire [17:0] c, d, v1, u1, s1;
-	assign a = 3 ;  // bits for shifting, a = 0.125
-	assign b =  2 ;  // bits for shifting, b = 0.25
-	assign c =  18'sh3_599A ; // -0.65  = dec2hex(1+bitcmp(ceil(0.65 * hex2dec('ffff')),18)) = 3599A
-	assign d =  18'sh0_147A ; // 0.08 = dec2hex(floor(0.08 * hex2dec('ffff'))) = 147A
-	assign tau = 4'h2;
-    
-	wire [1:0] state;
-	assign state = neuronCounter[1:0];
-    
-	wire [NN:0] neuronIndex;
-	assign neuronIndex = neuronCounter[NN+2:2];
-	
-	wire state1, state2, state3, state4;
-	assign state1 = (state == 2'h0);
-	assign state2 = (state == 2'h1);
-	assign state3 = (state == 2'h2);
-	assign state4 = (state == 2'h3);
-	
-	wire neuronWriteCount, readClock, neuronWriteEnable, dataValid;
-	assign neuronWriteCount = state1;	//increment neuronID (ram address)
-	assign readClock = state2;				//read RAM
-	assign neuronWriteEnable = state4; //(state3 | state4);	//write RAM
-	assign dataValid = (neuronCounter == 32'd0);  //(neuronIndex ==0) & state2; //(neuronIndex == 1);   //slight delay of positive edge to allow latch set-up times
-		
-    wire MN_spike;
-
-	Iz_neuron #(.NN(NN),.DELAY(10)) neuMN(v1,s1, a,b,c,d, i_postsyn_I[17:0] * i_gain_MN[17:0], neuron_clk, reset_sim, neuronIndex, neuronWriteEnable, readClock, tau, MN_spike);
-    
-    reg [15:0] raw_Ia_spikes, raw_II_spikes, raw_MN_spikes;
-	always @(negedge ti_clk) raw_MN_spikes <= {1'b0, neuronIndex[NN:2], MN_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
-	always @(negedge ti_clk) raw_Ia_spikes <= {1'b0, neuronIndex[NN:2], 1'b0, Ia_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
-//    always @(negedge ti_clk) raw_II_spikes <= {1'b0, neuronIndex[NN:2], 1'b0, 1'b0, II_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
-
-//
-//    assign raw_MN_spikes = {1'b0, neuronIndex[NN:2], MN_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
-//	assign raw_Ia_spikes = {1'b0, neuronIndex[NN:2], 1'b0, Ia_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
-//	assign raw_II_spikes = {1'b0, neuronIndex[NN:2], 1'b0, 1'b0, II_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
-    
-    // *** Count the spikes: rawspikes -> spike -> spike_count_out
-	wire    [31:0] i_MN_spk_cnt;
-    wire    clear_out;
-//    spikecnt count_rawspikes
-//	 (		.spike(MN_spike), 
-//			.slow_clk(sim_clk), 
-//			.fast_clk(neuron_clk),
-//            .int_cnt_out(i_MN_spk_cnt),
-//			.reset(reset_sim),
-//            .clear_out(clear_out) );   
-
-    spike_counter count_rawspikes
-    (   .spike(MN_spike), 
-        .slow_clk(sim_clk), 
-        .reset(reset_sim),
-        .int_cnt_out(i_MN_spk_cnt),
-        .clear_out(clear_out) );
-            
-    // *** Shadmehr muscle: spike_count_out => f_active_state => f_total_force
-    wire    [31:0]  f_total_force, f_active_state, f_MN_spk_cnt;
-    shadmehr_muscle muscle_for_test
-    (   .spike_cnt(i_MN_spk_cnt*gain),
-        .pos(f_muscle_len),  // muscle length
-        //.vel(current_vel),
-        .vel(32'd0),
-        .clk(sim_clk),
-        .reset(reset_sim),
-        .total_force_out(f_total_force),
-        .current_A(f_active_state),
-        .current_fp_spikes(f_MN_spk_cnt)
-    );       
 
     // *** EMG
 //    wire [17:0] si_emg;
