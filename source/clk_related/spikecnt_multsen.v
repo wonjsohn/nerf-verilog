b3f3e72bd05219ebae33b681d5e0fb055f3e92cc
sirish <sirish@localhost>
2012-02-28 13:01:09 -0800

    snapshot

--- source/clk_related/spikecnt_multsen.v
+++ source/clk_related/spikecnt_multsen.v
@@ -1,15 +1,22 @@
-module spike_counter(spike, int_cnt_out, slow_clk, reset, clear_out);
+module spike_counter(spike, int_cnt_out, slow_clk, reset);
     input   spike, slow_clk, reset;
     output  reg    [31:0]  int_cnt_out;
     reg     [31:0]  cnt;
-	reg slow_clk_up; 
-    output clear_out;
-    assign clear_out = slow_clk_up;
-	 
+	//reg slow_clk_up; 
+    //output clear_out;
+    //assign clear_out = slow_clk_up;
+
+
+
+// does nothing, cannot have self-triggering block
+/*	 
     always @(posedge reset or posedge slow_clk or posedge slow_clk_up) begin
         if (reset) begin 
             slow_clk_up <= 1'b0;
         end 
+        else if (slow_clk_up) begin
+            slow_clk_up <= 1'b0;
+        end
         else if (slow_clk) begin
             slow_clk_up <= 1'b1;
         end
@@ -17,18 +24,18 @@ module spike_counter(spike, int_cnt_out, slow_clk, reset, clear_out);
             slow_clk_up <= 1'b0;
         end
     end 
-
+*/
      
-    always @(posedge reset or posedge slow_clk_up or posedge spike) begin
+    always @(posedge reset or posedge slow_clk or posedge spike) begin
 	  if (reset) begin
 			cnt <= 32'd0;
 			int_cnt_out <= 32'd0;	
 	  end
-	  else if (slow_clk_up && spike) begin   // SPIKE HIGH and SLOW_CLK UP.
+	  else if (slow_clk && spike) begin   // SPIKE HIGH and SLOW_CLK UP.
 			int_cnt_out <= cnt;
 			cnt <= 32'd1;  // add one spike                 
 	  end   
-	  else if (slow_clk_up) begin  // SLOW CLK UP, NO SPIKE
+	  else if (slow_clk) begin  // SLOW CLK UP, NO SPIKE
 			int_cnt_out <= cnt;
 			cnt <= 32'd0;    // count being renewed at every posedge of slow clock = read.
 	  end
