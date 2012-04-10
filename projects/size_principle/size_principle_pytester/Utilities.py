3c1fab8b9dc46c338e556c83cae1e5b2efaf57b0
C. Minos Niu <minos.niu@gmail.com>
2012-04-10 11:51:09 -0700

    Some patches

--- projects/size_principle/size_principle_pytester/Utilities.py
+++ projects/size_principle/size_principle_pytester/Utilities.py
@@ -37,4 +37,12 @@ def ConvertType(val, fromType, toType):
     return unpack(toType, pack(fromType, val))[0]
     
 
+import sys, re
+def interp(string):
+  locals  = sys._getframe(1).f_locals
+  globals = sys._getframe(1).f_globals
+  for item in re.findall(r'#\{([^}]*)\}', string):
+    string = string.replace('#{%s}' % item,
+                            str(eval(item, globals, locals)))
+  return string
 
