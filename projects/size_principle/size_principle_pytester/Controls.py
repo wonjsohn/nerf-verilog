7a45860308a7b910d79d7dd3354684c6388d11cf
wonjsohn <wonjsohn@localhost>
2012-05-16 12:02:15 -0700

    GOAL: test new izh neuron to drive EMG.

---
+++ projects/size_principle/size_principle_pytester/Controls.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module implementing Control.
+"""
+
+from PyQt4.QtGui import QDialog
+from PyQt4.QtCore import pyqtSignature
+from PyQt4.QtCore import QTimer,  SIGNAL, SLOT, Qt,  QRect
+from PyQt4 import QtCore, QtGui
+
+from Utilities import *
+from generate_sin import gen as gen_sin
+from generate_tri import gen as gen_tri
+from generate_spikes import spike_train
+from generate_sequence import gen as gen_ramp
+from math import floor
+
+from Fpga import Model
+from Display import View
+
+class CtrlChannel:
+    def __init__(self, hostDialog, name, id, type, value = 0.0):
+        exec interp('self.id = #{id}')
+        exec interp('self.currVal = #{value}')
+        exec interp('self.type = type')
+
+        exec interp('self.doubleSpinBox = QtGui.QDoubleSpinBox(hostDialog)')
+        exec interp('self.doubleSpinBox.setGeometry(QtCore.QRect(230, #{id} * 35, 105, 30))')
+        exec interp('self.doubleSpinBox.setProperty("value", value)')
+        exec interp('self.doubleSpinBox.setObjectName("param_#{name}")')
+        
+        exec interp('self.label = QtGui.QLabel(hostDialog)')
+        exec interp('self.label.setObjectName("label_#{name}")')
+        exec interp('self.label.setText("#{name}")')        
+        exec interp('self.label.setGeometry(QtCore.QRect(350, #{id} * 35, 105, 30))')
+
+from Ui_Controls import Ui_Dialog
+class User(QDialog, Ui_Dialog):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent = None):
+        """
+        Constructor
+        """
+#        QDialog.__init__(self, parent, Qt.FramelessWindowHint)
+        QDialog.__init__(self, parent)
+        self.setupUi(self)
+        
+        self.nerfModel = Model()
+        self.dispView = View(None, VIEWER_REFRESH_RATE, CHIN_PARAM)
+
+        self.dispView.show()
+        self.data = []
+        self.isLogData = False
+        
+        # Create float_spin for each input channel
+        self.ctrl_all = []
+        for (trig_id, name, type, value) in CHOUT_PARAM:
+            exec interp('self.ctrl_#{name} = CtrlChannel(hostDialog=self, name=name, id=trig_id, type=type, value=value)')
+            exec interp('self.connect(self.ctrl_#{name}.doubleSpinBox, SIGNAL("editingFinished()"), self.onNewWireIn)')
+            exec interp('self.connect(self.ctrl_#{name}.doubleSpinBox, SIGNAL("valueChanged(double)"), self.onNewWireIn)')
+            exec interp('self.ctrl_all.append(self.ctrl_#{name})')        
+        
+        # Timer for pulling data, separated from timer_display
+        self.timer = QTimer(self)
+        self.connect(self.timer, SIGNAL("timeout()"), self.onSyncData)       
+        self.timer.start(VIEWER_REFRESH_RATE * 1.5)
+        
+        
+        self.on_horizontalSlider_valueChanged(5)
+        
+
+    def onSyncData(self):
+        """
+        Core function of Controller, which polls data from Model(fpga) and sends them to Viewer.
+        """
+        newData = []
+        for xaddr, xtype in zip(DATA_OUT_ADDR, CH_TYPE):
+            #newData[i] = self.nerfModel.ReadFPGA(DATA_OUT_ADDR[i], CH_TYPE[i])
+#            if i == 3: 
+#                newData[i] = newData[i] / 100
+            newData.append(max(-65535, min(65535, self.nerfModel.ReadFPGA(xaddr, xtype))))
+            
+        newSpike = self.nerfModel.ReadPipe(0xA1, 8000) # read ## bytes
+        #newSpike = "" # read ## bytes
+        
+        self.dispView.newDataIO(newData, newSpike)
+        if (self.isLogData):
+            self.data.append(newData)
+        
+    def onClkRate(self, value):   
+        """ value = how many times of 1/10 real-time
+        """
+        newHalfCnt = 10 * 200 * (10 **6) / SAMPLING_RATE / NUM_NEURON / value / 2 / 4
+        self.nerfModel.SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
+        
+    def onNewWireIn(self):
+        for ctrl in self.ctrl_all:
+            newWireIn = ctrl.doubleSpinBox.value()
+            if newWireIn != ctrl.currVal:
+                ctrl.currValue = newWireIn
+                if (ctrl.type == 'int32'):
+                    bitVal = ConvertType(floor(newWireIn),  fromType = 'i',  toType = 'I')
+#                    print bitVal
+                elif (ctrl.type == 'float32'):
+                    bitVal = ConvertType(newWireIn, fromType = 'f', toType = 'I')
+                self.nerfModel.SendPara(bitVal = bitVal, trigEvent = ctrl.id)
+
+    def plotData(self, data):
+        from pylab import plot, show, subplot
+        from scipy.io import savemat, loadmat
+        import numpy as np
+        
+        if (data != []):
+            forplot = np.array(data)
+            for i in xrange(NUM_CHANNEL):
+                subplot(NUM_CHANNEL, 1, i+1)
+                plot(forplot[:, i])
+            show()
+            savemat("./matlab_cmn.mat", {"lce": forplot[:, 0], "Ia": forplot[:, 1], \
+                                         "II": forplot[:, 2], "force": forplot[:, 3], \
+                                         "emg": forplot[:, 4]})
+   
+    @pyqtSignature("QString")
+    def on_comboBox_activated(self, p0):
+        """
+        Slot documentation goes here.
+        """
+        choice = p0
+        if choice == "Spike Train 1Hz":
+#            pipeInData = spike_train(firing_rate = 1) 
+            pipeInData = gen_sin(F = 1.0, AMP = 100.0,  T = 2.0)
+        elif choice == "Spike Train 10Hz":
+#            pipeInData = spike_train(firing_rate = 10)      
+#            pipeInData = gen_sin(F = 4.0, AMP = 0.3)
+            pipeInData = gen_tri(T = 2.0) 
+            
+        elif choice == "Spike Train 20Hz":
+#            pipeInData = gen_tri() 
+#            pipeInData = gen_ramp(T = [0.0, 0.1, 0.2, 0.8, 0.9, 2.0], L = [1.0, 1.0, 1.1, 1.1, 1.0, 1.0], FILT = False)
+            pipeInData = gen_ramp(T = [0.0, 0.2, 0.8, 1.2,  1.8,  2.0], L = [0,  0,  1500, 1500, 0, 0], FILT = False)
+#            pipeInData = spike_train(firing_rate = 100) 
+        
+        self.nerfModel.SendPipe(pipeInData)
+
+    
+    @pyqtSignature("int")
+    def on_horizontalSlider_sliderMoved(self, position):
+        """
+        Slot documentation goes here.
+        """
+        self.onClkRate(position)
+            
+    @pyqtSignature("bool")
+    def on_pushButton_2_clicked(self, checked):
+        """
+        Slot documentation goes here.
+        """
+        self.dispView.close()
+        self.plotData(self.data)
+    
+    @pyqtSignature("int")
+    def on_horizontalSlider_valueChanged(self, value):
+        """
+        Slot documentation goes here.
+        """
+        self.onClkRate(value)
+    
+    @pyqtSignature("bool")
+    def on_pushButton_5_clicked(self, checked):
+        """
+        Slot documentation goes here.
+        """
+        newResetSim = checked
+        self.nerfModel.SendButton(newResetSim, BUTTON_RESET_SIM)
+    
+    @pyqtSignature("bool")
+    def on_pushButton_4_clicked(self, checked):
+        """
+        Slot documentation goes here.
+        """
+        newResetGlobal = checked
+        self.nerfModel.SendButton(newResetGlobal, BUTTON_RESET)
+    
+    @pyqtSignature("bool")
+    def on_pushButtonData_clicked(self, checked):
+        """
+        Slot documentation goes here.
+        """
+        self.isLogData = checked
---
+++ source/neuron_pool.v
@@ -0,0 +1,165 @@
+`timescale 1ns / 1ps
+`default_nettype none
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Design Name: 
+// Module Name:    neuron_pool.v
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+
+// 
+module neuron_pool (//(f_muscle_length, f_rawfr_Ia, f_pps_coef_Ia, gain, sim_clk, neuron_clk, reset_sim, f_total_force);
+    //input   [31:0]  vel,            // change of muscle length
+    input   wire [31:0]  f_rawfr_Ia,     //
+    input   wire [31:0]  f_pps_coef_Ia,  //
+    input   wire [31:0]  half_cnt,
+    input   wire rawclk,
+
+    input   wire ti_clk,
+    input   wire reset_sim,
+    input   wire signed [31:0] i_gain_MN,
+    input   wire [NN+2:0] neuronCounter,
+
+    output  wire MN_spike,
+    output  wire [15:0] spkid_MN,
+	 // debug
+	 output reg  [31:0] i_current_out,
+	 output wire signed [31:0] i_synI_rand_out,
+	 output wire signed [31:0] i_postsyn_I_out,
+	 output wire signed [31:0] i_gain_MN_used
+    );
+
+    parameter NN = 8; // 2^(NN+1) = NUM_NEURON
+    
+    //Locally generate neuron_clk
+    reg neuron_clk;
+    reg [31:0] delay_cnt;
+    always @ (posedge rawclk) begin
+        if (delay_cnt < half_cnt) begin
+            neuron_clk <= neuron_clk;
+            delay_cnt <= delay_cnt + 1;
+        end
+        else begin
+            neuron_clk<= ~neuron_clk;
+            delay_cnt <= 0;
+        end
+    end
+
+    // *** Izhikevich: f_fr_Ia => spikes
+    // *** Convert float_fr to int_I1
+
+    wire [31:0] f_fr_Ia;
+    wire [31:0] i_synI_Ia;
+
+	mult scale_pps_Ia( .x(f_rawfr_Ia), .y(f_pps_coef_Ia), .out(f_fr_Ia));
+    floor float_to_int_Ia( .in(f_fr_Ia), .out(i_synI_Ia) );
+    
+    wire Ia_spike, s_Ia;
+    wire signed [17:0] v_Ia;   // cell potentials
+        
+   
+//    Iz_neuron #(.NN(NN),.DELAY(10)) Ia_neuron
+//    (v_Ia,s_Ia, a,b,c,d, {i_synI_Ia[31], i_synI_Ia[16:0]}, neuron_clk, reset_sim, neuronIndex, neuronWriteEnable, readClock, 4'h2, Ia_spike, neuronWriteCount);
+////
+//    wire [31:0] f_fr_II;
+//    wire [31:0] i_synI_II;
+//    mult scale_pps_II( .x(f_rawfr_II), .y(f_pps_coef_II), .out(f_fr_II));
+//    floor float_to_int_II( .in(f_fr_II), .out(i_synI_II) );
+//    wire II_spike, s_II;
+//    wire signed [17:0] v_II;   // cell potentials
+//    Iz_neuron #(.NN(NN),.DELAY(10)) II_neuron
+//    (v_II,s_II, a,b,c,d, i_synI_II, neuron_clk, reset_sim, neuronIndex, neuronWriteEnable, readClock, 4'h2, II_spike);
+
+    //*** Synapse:: spike -> I   
+	wire signed [17:0]  I_out;
+    wire [31:0] i_postsyn_I;
+  
+	assign i_postsyn_I = {{14{I_out[17]}}, I_out};
+    
+    // *** izh-Motoneuron :: i_postsyn_I -> (MN_spike, rawspike)
+    
+
+    wire [17:0] v1, u1, s1;
+    
+   
+	wire [1:0] state;
+	assign state = neuronCounter[1:0];
+    
+	wire [NN:0] neuronIndex;
+	assign neuronIndex = neuronCounter[NN+2:2];
+	
+    //wire MN_spike;
+    
+    wire [31:0] rand_out;
+    rng rng_0(
+            .clk1(rawclk),
+            .clk2(rawclk),
+            .reset(reset_sim),
+            .out(rand_out)
+    );    
+    wire signed [31:0] i_synI_rand = {{26{1'b0}},rand_out[5:0]};
+	 ///debug
+	 //assign  i_current_out = ( {{14{i_postsyn_I[17]}}, i_postsyn_I[17:0]} + {{14{i_synI_rand[17]}}, i_synI_rand[17:0]} ) * i_gain_MN;
+	 //wire signed [17:0] i_init_current = i_postsyn_I[17:0] + i_synI_rand[17:0];
+	 
+	 //wire signed [31:0] i_init_current = {i_synI_Ia[31:5], i_synI_rand[4:0]};
+	 wire signed [31:0] i_init_current = i_synI_Ia + i_synI_rand;
+	 wire signed [63:0] i_current64 = i_init_current * i_gain_MN;
+	 
+	 //reg [31:0] i_current_out;
+	 always @(posedge neuron_clk or posedge reset_sim) begin
+		if (reset_sim) begin
+			i_current_out <= 32'd0;
+		end
+		else begin
+			i_current_out <= {i_current64[63],i_current64[30:0]};
+		end
+	 end
+	 //assign i_gain_MN_used = {{14{i_gain_MN18[17]}}, i_gain_MN18[17:0]};
+	 
+	 
+	 assign i_synI_rand_out =  {{14{1'b0}}, i_synI_rand[17:0]};
+	 assign i_postsyn_I_out = {{14{i_postsyn_I[17]}},i_postsyn_I[17:0]};
+
+	//Iz_neuron #(.NN(NN),.DELAY(10)) neuMN(v1,s1, a,b,c,d, i_current_out18 , neuron_clk, reset_sim, neuronIndex, neuronWriteEnable, readClock, tau, MN_spike, neuronWriteCount);
+    wire [31:0] v;
+    wire spike;
+    wire [127:0] population;
+    izneuron neuron_0(
+                .clk(neuron_clk),
+                .reset(reset_sim),
+                .I_in(i_current_out),                
+                .spike(MN_spike),                
+                .spkid(spkid_MN)
+    );    
+    //reg [15:0] raw_Ia_spikes, raw_II_spikes, raw_MN_spikes;
+	//always @(negedge ti_clk) raw_Ia_spikes <= {1'b0, neuronIndex[NN:2], 1'b0, Ia_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
+//    always @(negedge ti_clk) raw_II_spikes <= {1'b0, neuronIndex[NN:2], 1'b0, 1'b0, II_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
+
+//
+//    assign raw_MN_spikes = {1'b0, neuronIndex[NN:2], MN_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
+//	assign raw_Ia_spikes = {1'b0, neuronIndex[NN:2], 1'b0, Ia_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
+//	assign raw_II_spikes = {1'b0, neuronIndex[NN:2], 1'b0, 1'b0, II_spike, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0};
+    
+    // *** Count the spikes: rawspikes -> spike -> spike_count_out
+	
+   
+
+
+
+
+endmodule
+
