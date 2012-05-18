150893a480ebdc83bcf3efc2aee1b47a04f3c7e7
wonjsohn <wonjsohn@localhost>
2012-05-18 13:25:26 -0700

    (Minos) Ready to wrap fpmul as mult()

--- source/neuron_pool.v
+++ source/neuron_pool.v
@@ -74,14 +74,14 @@ module neuron_pool (//(f_muscle_length, f_rawfr_Ia, f_pps_coef_Ia, gain, sim_clk
     
     wire [31:0] f_randn_F0 = {12'h3F8, rand_out[19:0]};
     wire [31:0] f_rand_Ia_F0; 
-    //mult get_rand_Ia( .x(f_rawfr_Ia), .y(f_randn), .out(f_rand_Ia_F0));
+    mult get_rand_Ia( .x(f_rawfr_Ia), .y(f_randn), .out(f_rand_Ia_F0));
     wire [4:0] flag_rand_Ia;
-    fpmul get_rand_Ia( .a(f_rawfr_Ia), .b(f_randn), .y(f_rand_Ia_F0), .control(5'h02), .flags(flag_rand_Ia));
+    //fpmul get_rand_Ia( .a(f_rawfr_Ia), .b(f_randn), .y(f_rand_Ia_F0), .control(5'h00), .flags(flag_rand_Ia));
     //assign f_rand_Ia = {f_rawfr_Ia[31], {f_rawfr_Ia[30:23]}, i23_rand};    
 
-	//mult scale_pps_Ia( .x(f_rand_Ia), .y(f_pps_coef_Ia), .out(f_fr_Ia_F0));
+	mult scale_pps_Ia( .x(f_rand_Ia), .y(f_pps_coef_Ia), .out(f_fr_Ia_F0));
     wire [4:0] flag_fr_Ia;
-    fpmul scale_pps_Ia( .a(f_rand_Ia), .b(f_pps_coef_Ia), .y(f_fr_Ia_F0), .control(5'h03), .flags(flag_fr_Ia));
+    //fpmul scale_pps_Ia( .a(f_rand_Ia_F0), .b(f_pps_coef_Ia), .y(f_fr_Ia_F0), .control(5'h00), .flags(flag_fr_Ia));
     
 	//mult scale_pps_Ia( .x(f_norm_Ia), .y(f_pps_coef_Ia), .out(f_fr_Ia));
     
@@ -124,8 +124,10 @@ module neuron_pool (//(f_muscle_length, f_rawfr_Ia, f_pps_coef_Ia, gain, sim_clk
 		else begin
 			//i_current_out <= i_current_F0;
 			i_current_out <= i_synI_Ia;
-            f_fr_Ia <= (flag_fr_Ia[2]) ? f_fr_Ia_F0 : f_fr_Ia; 
-            f_rand_Ia <= (flag_rand_Ia[2]) ? f_rand_Ia_F0 : f_rand_Ia;        
+//            f_fr_Ia <= (flag_fr_Ia[2]) ? f_fr_Ia_F0 : f_fr_Ia; 
+//            f_rand_Ia <= (flag_rand_Ia[2]) ? f_rand_Ia_F0 : f_rand_Ia;      
+            f_fr_Ia <= f_fr_Ia_F0; 
+            f_rand_Ia <= f_rand_Ia_F0;                  
             f_randn <= f_randn_F0;
 		end
 	 end
