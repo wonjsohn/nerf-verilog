4d52815802d7b3e7fd00d61e5210c828f30a4f1d
wonjsohn <wonjsohn@localhost>
2012-08-02 18:55:06 -0700

    (Minos) Relationship between neuron_clk and sim_clk is correct. BUG: Neurons are not firing correctly, EMG... are wrong too

--- source/clk_related/gen_clk.v
+++ source/clk_related/gen_clk.v
@@ -1,18 +1,20 @@
 module gen_clk(rawclk, reset, half_cnt, clk_out1, clk_out2, clk_out3, int_neuron_cnt_out);
     parameter NN = 6; // 2^(NN+1) = NUM_NEURON
     parameter SR = 10; // 2^SR = SAMPLING_RATE
+    parameter CYCLE = 1; // 2^CYCLE = Iterations cycles needed for each neuron
     input rawclk;
     input reset;
     input [31:0] half_cnt;
     output reg clk_out1, clk_out2, clk_out3;
     output [31:0] int_neuron_cnt_out;
 
-    reg [31:0] delay_cnt;
+    reg [31:0] delay_cnt, neuron_cnt;
 
     always @ (posedge rawclk) begin
 	     if (reset) begin
             clk_out1 <= 0;
 		  end 
+		  
 		  else begin
 			  if (delay_cnt < half_cnt) begin
 					clk_out1 <= clk_out1;
@@ -23,12 +25,14 @@ module gen_clk(rawclk, reset, half_cnt, clk_out1, clk_out2, clk_out3, int_neuron
 					delay_cnt <= 0;
 			  end
 		  end
-    end
-
-	reg [NN+2:0] neuronCounter;
+    end	 
+	 
+	 
+	 
+	reg [NN+CYCLE:0] neuronCounter;
 	wire [NN:0] neuronIndex;
 
-	assign neuronIndex = neuronCounter[NN+2:2];
+	assign neuronIndex = neuronCounter[NN+CYCLE:CYCLE];
 
 	always @ (posedge clk_out1 or posedge reset)
 	begin
@@ -43,6 +47,7 @@ module gen_clk(rawclk, reset, half_cnt, clk_out1, clk_out2, clk_out3, int_neuron
                         (neuronIndex == 9'd170)};
             end
 	end
-
+	 
+	 	 
     assign int_neuron_cnt_out = neuronCounter;
 endmodule
