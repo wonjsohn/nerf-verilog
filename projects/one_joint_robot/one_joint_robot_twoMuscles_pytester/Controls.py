c6e5f33a4ef1432c22902e063b27deb2eba5494f
eric <eric@fpga-dell>
2012-08-28 21:37:14 -0700

    snapshot: python reads from two Fpga boards

--- projects/one_joint_robot/one_joint_robot_twoMuscles_pytester/Controls.py
+++ projects/one_joint_robot/one_joint_robot_twoMuscles_pytester/Controls.py
@@ -16,7 +16,14 @@ from generate_spikes import spike_train
 from generate_sequence import gen as gen_ramp
 from math import floor
 
-from Fpga import Model
+from Fpga import FpgaDevice
+import platform
+arch = platform.architecture()[0]
+if arch == "32bit":
+    from opalkelly_32bit import ok
+elif arch == "64bit":
+    from opalkelly_64bit import ok
+    
 from Display import View
 
 class CtrlChannel:
@@ -38,7 +45,7 @@ class CtrlChannel:
              
              
 from Ui_Controls import Ui_Dialog
-class User1(QDialog, Ui_Dialog):
+class User(QDialog, Ui_Dialog):
     """
     Class documentation goes here.
     """
@@ -50,7 +57,34 @@ class User1(QDialog, Ui_Dialog):
         QDialog.__init__(self, parent)
         self.setupUi(self)
         
-        self.nerfModel = Model()
+        rawxem = ok.FrontPanel()
+        #count = 2
+        count = rawxem.GetDeviceCount()
+        print count
+    
+        
+        
+        self.xemList = []
+        
+        
+        
+        BIT_FILE_LIST = [BIT_FILE,  BIT_FILE2]
+        #BIT_FILE_LIST = [BIT_FILE]
+        for i in xrange(count):
+            
+            serX = rawxem.GetDeviceListSerial(i)
+            print "serial = ",  serX    
+            self.nerfModel = FpgaDevice(serX,  BIT_FILE_LIST[i])
+            self.xemList.append(self.nerfModel)
+      
+
+            
+        
+
+        # SEND SINEWAVE ALWAYS 
+        pipeInData = gen_sin(F = 1.0, AMP = 100.0,  T = 2.0)
+        self.xemList[1].SendPipe(pipeInData)
+        
         
         #pipeInData = gen_sin(F = 1.0, AMP = 100.0,  T = 2.0)
         #self.nerfModel.SendPipe(pipeInData)
@@ -83,13 +117,22 @@ class User1(QDialog, Ui_Dialog):
         """
         Core function of Controller, which polls data from Model(fpga) and sends them to Viewer.
         """
-        newData = []
+        newData1 = []
+        newData2 = []
+        
         for xaddr, xtype in zip(DATA_OUT_ADDR, CH_TYPE):
             #newData[i] = self.nerfModel.ReadFPGA(DATA_OUT_ADDR[i], CH_TYPE[i])
 #            if i == 3: 
 #                newData[i] = newData[i] / 100
-            newData.append(max(-16777216, min(16777216, self.nerfModel.ReadFPGA(xaddr, xtype))))
-            print newData[0::6]   # printing 
+            #newData.append(max(-16777216, min(16777216, self.nerfModel.ReadFPGA(xaddr, xtype))))
+            newData1.append(max(-16777216, min(16777216, self.xemList[0].ReadFPGA(xaddr, xtype))))
+
+            #print newData1[0::6]   # printing 
+            
+        newData2.append(max(-16777216, min(16777216, self.xemList[1].ReadFPGA(0x20, 'float32'))))  # sine wave
+        #print self.xemList[1]
+        print newData1
+        print newData2#[0::1]   # printing 
             #        newSpike1 = self.nerfModel.ReadPipe(0xA0, 5000) # read ## bytes
 #        newSpike2 = self.nerfModel.ReadPipe(0xA1, 5000) # read ## bytes
 #        newSpike3 = self.nerfModel.ReadPipe(0xA2, 5000) # read ## bytes
@@ -104,10 +147,10 @@ class User1(QDialog, Ui_Dialog):
 
         #newSpike = "" # read ## bytes
         
-        self.dispView.newDataIO(newData, [newSpike1, newSpike2, newSpike3, newSpike4, newSpike5])
+        self.dispView.newDataIO(newData1, [newSpike1, newSpike2, newSpike3, newSpike4, newSpike5])
         #self.dispView.newDataIO(newData, [])
         if (self.isLogData):
-            self.data.append(newData)
+            self.data.append(newData1)
         
     def onClkRate(self, value):   
         """ value = how many times of 1/10 real-time
@@ -120,7 +163,8 @@ class User1(QDialog, Ui_Dialog):
         print 'halfcnt=%d' %newHalfCnt
         print 'value=%d' %value
 
-        self.nerfModel.SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
+        #self.nerfModel.SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
+        self.xemList[0].SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
         
     def onNewWireIn(self):
         for ctrl in self.ctrl_all:
@@ -132,7 +176,7 @@ class User1(QDialog, Ui_Dialog):
 #                    print bitVal
                 elif (ctrl.type == 'float32'):
                     bitVal = ConvertType(newWireIn, fromType = 'f', toType = 'I')
-                self.nerfModel.SendPara(bitVal = bitVal, trigEvent = ctrl.id)
+                self.xemList[0].SendPara(bitVal = bitVal, trigEvent = ctrl.id)
                 
 
 
@@ -172,7 +216,7 @@ class User1(QDialog, Ui_Dialog):
 #                pipeInData = gen_ramp(T = [0.0, 0.2, 0.25, 1.75,  1.8,  2.0], L = [1.0,  1.0,  5000.0, 5000.0, 1.0, 1.0], FILT = False)  # abrupt rise / fall
 #            pipeInData = spike_train(firing_rate = 1000) 
         
-        self.nerfModel.SendPipe(pipeInData)
+        self.xemList[0].SendPipe(pipeInData)
 
     
     @pyqtSignature("int")
@@ -189,6 +233,7 @@ class User1(QDialog, Ui_Dialog):
         """
         self.dispView.close()
         self.plotData(self.data)
+
     
     @pyqtSignature("int")
     def on_horizontalSlider_valueChanged(self, value):
@@ -203,7 +248,7 @@ class User1(QDialog, Ui_Dialog):
         Slot documentation goes here.
         """
         newResetSim = checked
-        self.nerfModel.SendButton(newResetSim, BUTTON_RESET_SIM)
+        self.xemList[0].SendButton(newResetSim, BUTTON_RESET_SIM)
     
     @pyqtSignature("bool")
     def on_pushButton_4_clicked(self, checked):
@@ -211,7 +256,7 @@ class User1(QDialog, Ui_Dialog):
         Slot documentation goes here.
         """
         newResetGlobal = checked
-        self.nerfModel.SendButton(newResetGlobal, BUTTON_RESET)
+        self.xemList[0].SendButton(newResetGlobal, BUTTON_RESET)
     
     @pyqtSignature("bool")
     def on_pushButtonData_clicked(self, checked):
@@ -231,187 +276,3 @@ class User1(QDialog, Ui_Dialog):
         
 
 
-
-
-class User2(QDialog, Ui_Dialog):
-    """
-    Class documentation goes here.
-    """
-    def __init__(self, parent = None):
-        """
-        Constructor
-        """
-#        QDialog.__init__(self, parent, Qt.FramelessWindowHint)
-        QDialog.__init__(self, parent)
-        self.setupUi(self)
-        
-        self.nerfModel2 = Model()
-        self.dispView = View(None, VIEWER_REFRESH_RATE, CHIN_PARAM)
-
-        self.dispView.show()
-        self.data = []
-        self.isLogData = False
-        
-        # Create float_spin for each input channel
-        self.ctrl_all = []
-        for (trig_id, name, type, value) in CHOUT_PARAM:
-            exec interp('self.ctrl_#{name} = CtrlChannel(hostDialog=self, name=name, id=trig_id, type=type, value=value)')
-            exec interp('self.connect(self.ctrl_#{name}.doubleSpinBox, SIGNAL("editingFinished()"), self.onNewWireIn)')
-            exec interp('self.connect(self.ctrl_#{name}.doubleSpinBox, SIGNAL("valueChanged(double)"), self.onNewWireIn)')
-            exec interp('self.ctrl_all.append(self.ctrl_#{name})')        
-        
-        # Timer for pulling data, separated from timer_display
-        self.timer = QTimer(self)
-        self.connect(self.timer, SIGNAL("timeout()"), self.onSyncData)       
-        self.timer.start(VIEWER_REFRESH_RATE )
-        
-        
-        self.on_horizontalSlider_valueChanged(5)
-        
-
-    def onSyncData(self):
-        """
-        Core function of Controller, which polls data from Model(fpga) and sends them to Viewer.
-        """
-        newData = []
-        for xaddr, xtype in zip(DATA_OUT_ADDR, CH_TYPE):
-            #newData[i] = self.nerfModel.ReadFPGA(DATA_OUT_ADDR[i], CH_TYPE[i])
-#            if i == 3: 
-#                newData[i] = newData[i] / 100
-            newData.append(max(-16777216, min(16777216, self.nerfModel2.ReadFPGA(xaddr, xtype))))
-            
-#        newSpike1 = self.nerfModel.ReadPipe(0xA0, 5000) # read ## bytes
-#        newSpike2 = self.nerfModel.ReadPipe(0xA1, 5000) # read ## bytes
-#        newSpike3 = self.nerfModel.ReadPipe(0xA2, 5000) # read ## bytes
-#        newSpike4 = self.nerfModel.ReadPipe(0xA3, 5000) # read ## bytes
-#        newSpike5 = self.nerfModel.ReadPipe(0xA4, 5000) # read ## bytes
-        newSpike1 = ""
-        newSpike2 = ""
-        newSpike3 = ""
-        newSpike4 = ""
-        newSpike5 = ""
-        
-        #newSpike = "" # read ## bytes
-        
-        self.dispView.newDataIO(newData, [newSpike1, newSpike2, newSpike3, newSpike4, newSpike5])
-        #self.dispView.newDataIO(newData, [])
-        if (self.isLogData):
-            self.data.append(newData)
-        
-    def onClkRate(self, value):   
-        """ value = how many times of 1/10 real-time
-        """
-        newHalfCnt = 1 * 200 * (10 **6) / SAMPLING_RATE / NUM_NEURON / (value*4) / 2 / 2
-        #newHalfCnt = 1
-        print 'halfcnt=%d' %newHalfCnt
-        print 'value=%d' %value
-
-        self.nerfModel2.SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
-        
-    def onNewWireIn(self):
-        for ctrl in self.ctrl_all:
-            newWireIn = ctrl.doubleSpinBox.value()
-            if newWireIn != ctrl.currVal:
-                ctrl.currValue = newWireIn
-                if (ctrl.type == 'int32'):
-                    bitVal = ConvertType(floor(newWireIn),  fromType = 'i',  toType = 'I')
-#                    print bitVal
-                elif (ctrl.type == 'float32'):
-                    bitVal = ConvertType(newWireIn, fromType = 'f', toType = 'I')
-                self.nerfModel2.SendPara(bitVal = bitVal, trigEvent = ctrl.id)
-                
-
-
-    def plotData(self, data):
-        from pylab import plot, show, subplot
-        from scipy.io import savemat, loadmat
-        import numpy as np
-        
-        if (data != []):
-            forplot = np.array(data)
-            for i in xrange(NUM_CHANNEL):
-                subplot(NUM_CHANNEL, 1, i+1)
-                plot(forplot[:, i])
-            show()
-            savemat("./matlab_cmn.mat", {"lce": forplot[:, 0], "Ia": forplot[:, 1], \
-                                         "II": forplot[:, 2], "force": forplot[:, 3], \
-                                         "emg": forplot[:, 4]})
-   
-    @pyqtSignature("QString")
-    def on_comboBox_activated(self, p0):
-        """
-        Slot documentation goes here.
-        """
-        choice = p0
-        if choice == "Spike Train 1Hz":
-#            pipeInData = spike_train(firing_rate = 1) 
-            pipeInData = gen_sin(F = 1.0, AMP = 100.0,  T = 2.0)
-        elif choice == "Spike Train 10Hz":
-#            pipeInData = spike_train(firing_rate = 10)      
-#            pipeInData = gen_sin(F = 4.0, AMP = 0.3)
-            pipeInData = gen_tri(T = 2.0) 
-            
-        elif choice == "Spike Train 20Hz":
-#            pipeInData = gen_tri() 
-            pipeInData = gen_ramp(T = [0.0, 0.1, 0.2, 0.8, 0.9, 2.0], L = [1.0, 1.0, 1.3, 1.3, 1.0, 1.0], FILT = False)
-#            pipeInData = gen_ramp(T = [0.0, 0.4, 1.5, 1.55,  1.6,  2.0], L = [0,  0,  15000, 15000, 0, 0], FILT = False)
-#                pipeInData = gen_ramp(T = [0.0, 0.2, 0.25, 1.75,  1.8,  2.0], L = [1.0,  1.0,  5000.0, 5000.0, 1.0, 1.0], FILT = False)  # abrupt rise / fall
-#            pipeInData = spike_train(firing_rate = 1000) 
-        
-        self.nerfModel2.SendPipe(pipeInData)
-
-    
-    @pyqtSignature("int")
-    def on_horizontalSlider_sliderMoved(self, position):
-        """
-        Slot documentation goes here.
-        """
-        self.onClkRate(position)
-            
-    @pyqtSignature("bool")
-    def on_pushButton_2_clicked(self, checked):
-        """
-        Slot documentation goes here.
-        """
-        self.dispView.close()
-        self.plotData(self.data)
-    
-    @pyqtSignature("int")
-    def on_horizontalSlider_valueChanged(self, value):
-        """
-        Slot documentation goes here.
-        """
-        self.onClkRate(value)
-    
-    @pyqtSignature("bool")
-    def on_pushButton_5_clicked(self, checked):
-        """
-        Slot documentation goes here.
-        """
-        newResetSim = checked
-        self.nerfModel2.SendButton(newResetSim, BUTTON_RESET_SIM)
-    
-    @pyqtSignature("bool")
-    def on_pushButton_4_clicked(self, checked):
-        """
-        Slot documentation goes here.
-        """
-        newResetGlobal = checked
-        self.nerfModel2.SendButton(newResetGlobal, BUTTON_RESET)
-    
-    @pyqtSignature("bool")
-    def on_pushButtonData_clicked(self, checked):
-        """
-        Slot documentation goes here.
-        """
-        self.isLogData = checked
-
-
-#    @pyqtSignature("bool")
-#    def on_checkbox_checked(self, state):
-#        """
-#        Slot documentation goes here.
-#        """
-#        if state == QtCore.Qt.Checked:
-#            self.nerfModel.SendCheck()
-        
