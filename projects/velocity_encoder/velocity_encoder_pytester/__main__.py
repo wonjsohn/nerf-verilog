00e47fa2121d428a4c287782107dd294b3482985
C. Minos Niu <C. Minos Niu@localhost>
2012-11-19 15:15:01 -0800

    Snapshot

--- projects/velocity_encoder/velocity_encoder_pytester/__main__.py
+++ projects/velocity_encoder/velocity_encoder_pytester/__main__.py
@@ -4,10 +4,8 @@
 if __name__ == "__main__":
     import sys, PyQt4
     sys.path.append('../../../source/py/')
-    from Controls import User1#,  User2
+    from Controls import User1
     app = PyQt4.QtGui.QApplication(sys.argv)
     user1 = User1()
-    #user2 = User2()
     user1.show()
-    #user2.show()
     sys.exit(app.exec_())
