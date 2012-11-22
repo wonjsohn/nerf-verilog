47c1cf4f5ad558e1251eea0cf8536b9ba76f524f
C. Minos Niu <C. Minos Niu@localhost>
2012-11-21 18:08:32 -0800

    * Metaprograming in Python, dynamically created QtWidgets for each channel

--- source/py/__main__.py
+++ source/py/__main__.py
@@ -3,11 +3,26 @@
 
 import sys, PyQt4
 from PyQt4.QtGui import QFileDialog
-from GuiPanel import SingleDutTester # Controller in MVC
+from PyQt4.QtCore import pyqtSignature, pyqtSlot
+
+from PyQt4.QtCore import QTimer,  SIGNAL, SLOT, Qt,  QRect
+from GuiPanel import SingleDutTester, CtrlChannel # Controller in MVC
 from Fpga import SomeFpga # Model in MVC
 from Display import View # Viewer in MVC
 from Utilities import *
 import os
+import types
+import inspect
+
+updater_macro = """
+@pyqtSlot('double')
+def #{realUpdaterName}(self, value): 
+    print value
+    frame = inspect.currentframe()
+    print inspect.getframeinfo(frame)
+"""
+
+
 
 if __name__ == "__main__":        
     app = PyQt4.QtGui.QApplication(sys.argv)
@@ -30,9 +45,15 @@ if __name__ == "__main__":
     
     # Customize a curve plotting window 
     dispWindow = View(VIEWER_REFRESH_RATE, CHIN_PARAM)
-        
+    
     # Pass device and dispView to the main GUI
-    testerGui = SingleDutTester(xem, dispWindow)
+    testerGui = SingleDutTester(xem, dispWindow, TESTABLE_INPUTS)
+    for name in testerGui.ctrl_all:
+        eachCtrl = testerGui.ctrl_all[name]
+        realUpdaterName = "__onNewValue__" + name
+        exec interp(updater_macro)
+        testerGui.__dict__[realUpdaterName] = types.MethodType(eval(realUpdaterName), testerGui)        
+        testerGui.connect(eachCtrl.doubleSpinBox, SIGNAL("valueChanged(double)"), testerGui.__getattribute__(realUpdaterName))
     testerGui.show()
         
     sys.exit(app.exec_())
