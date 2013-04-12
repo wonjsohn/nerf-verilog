65e3896d6d21b6081d7442cd21ab4624ae1ac380
wonjsohn <wonjsohn@localhost>
2013-04-12 16:52:06 -0700

    snapshot

--- source/py/multC_tester/__main__.py
+++ source/py/multC_tester/__main__.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
     app = PyQt4.QtGui.QApplication(sys.argv)
     
 #    ROOT_PATH = QFileDialog.getExistingDirectory(None, "Path for the Verilog .bit file", os.getcwd() + "../../")
-  
-    ROOT_PATH = "C:\\Code\\nerf_verilog\\projects\\"
+
+    ROOT_PATH = "/home/eric/nerf_verilog_eric/projects/"
 #    ROOT_PATH = "/home/eric/"
 #    PROJECT_NAME1 = "one_joint_parameterSearch"
 #    PROJECT_NAME2 = "size_principle"
@@ -34,9 +34,10 @@ if __name__ == "__main__":
     PROJECT_NAME2 = "rack_mid_node1"
     PROJECT_NAME3 = "rack_mn_muscle"
     
-    PROJECT_PATH1 = ROOT_PATH + PROJECT_NAME1
-    PROJECT_PATH2 = ROOT_PATH + PROJECT_NAME2
-    PROJECT_PATH3 = ROOT_PATH + PROJECT_NAME3
+    PROJECT_PATH = []
+    PROJECT_PATH.append(ROOT_PATH + PROJECT_NAME1)
+#    PROJECT_PATH.append(ROOT_PATH + PROJECT_NAME2)
+    PROJECT_PATH.append(ROOT_PATH + PROJECT_NAME3)
     DEVICE_MODEL = "xem6010"
     
     #BITFILE_NAME = PROJECT_PATH1 + "/" + PROJECT_NAME + "_" + DEVICE_MODEL + ".bit"
@@ -45,12 +46,21 @@ if __name__ == "__main__":
     
     #sys.path.append(PROJECT_PATH)
     from config_test import NUM_NEURON, SAMPLING_RATE, FPGA_OUTPUT_B1, FPGA_OUTPUT_B2, FPGA_OUTPUT_B3,   USER_INPUT_B1,  USER_INPUT_B2,  USER_INPUT_B3
-
+    FPGA_OUTPUT_B = []
+    FPGA_OUTPUT_B.append(FPGA_OUTPUT_B1)
+    #FPGA_OUTPUT_B.append(FPGA_OUTPUT_B2)
+    FPGA_OUTPUT_B.append(FPGA_OUTPUT_B3)
+    USER_INPUT_B = []
+    USER_INPUT_B.append(USER_INPUT_B1)
+#    USER_INPUT_B.append(USER_INPUT_B2)
+    USER_INPUT_B.append(USER_INPUT_B3)
         
+
     # Connect to an OpalKelly device on USB
     # Bind the .bit file with the device
     # CONFIGURE MULTIPLE BOARDS 
     
+        
     ### Building M in MVC
     xemList = []
     testrun = ok.FrontPanel()
@@ -59,7 +69,8 @@ if __name__ == "__main__":
     print "Found ",  numFpga, " OpalKelly devices:"                        
     #xemSerialList = [testrun.GetDeviceListSerial(i) for i in xrange(numFpga)]
     #xemSerialList = ['124300046A', '12320003RM', '1201000216']
-    xemSerialList = ['12320003RN', '11160001CJ', '12430003T2']
+#    xemSerialList = ['12320003RN', '11160001CJ', '12430003T2']
+    xemSerialList = ['12320003RN', '12430003T2']
     print xemSerialList
     
     for idx,  name in enumerate(xemSerialList):
@@ -80,32 +91,24 @@ if __name__ == "__main__":
     ### Building V in MVC
     
     vList = []
-#    print PROJECT_PATH1,  xemList[0]
-#    print PROJECT_PATH2,  xemList[1]
-    dispWin = View(count = 0,  projectPath = PROJECT_PATH1,  nerfModel = xemList[0],  fpgaOutput = FPGA_OUTPUT_B1,  userInput = USER_INPUT_B1)
-    vList.append(dispWin)
-    dispWin = View(count = 1,  projectPath = PROJECT_PATH2, nerfModel = xemList[1],  fpgaOutput = FPGA_OUTPUT_B2,  userInput = USER_INPUT_B2)
-    vList.append(dispWin)
-    dispWin = View(count = 2,  projectPath = PROJECT_PATH3,  nerfModel = xemList[2],  fpgaOutput = FPGA_OUTPUT_B3,  userInput = USER_INPUT_B3)
-    vList.append(dispWin)    
-      
-    # display VIEW windows for each channel
-    vList[0].show()
-    vList[1].show()
-    vList[2].show()
-    
-    ### Building C::(M,V)->C in MVC
-    
-    cList = []
-    testerGui = SingleXemTester(xemList[0], vList[0], USER_INPUT_B1,  xem.HalfCountRealTime())
-    cList.append(testerGui)
-    testerGui = SingleXemTester(xemList[1], vList[1], USER_INPUT_B2,  xem.HalfCountRealTime())
-    cList.append(testerGui)
-    testerGui = SingleXemTester(xemList[2], vList[2], USER_INPUT_B3,  xem.HalfCountRealTime())
-    cList.append(testerGui)
-    
 
     
+    for i in xrange(len(xemList)):
+        dispWin = View(count = i,  projectPath = PROJECT_PATH[i],  nerfModel = xemList[i],  fpgaOutput = FPGA_OUTPUT_B[i],  userInput = USER_INPUT_B[i])
+        vList.append(dispWin)
+    print "vList = ",  vList
+      
+    # display VIEW windows for each channel
+    for i in xrange(len(xemList)):
+        vList[i].show()
+    
+    ### Building C::(M,V)->C in MVC
+    cList = []
+    for i in xrange(len(xemList)):
+        testerGui = SingleXemTester(xemList[i], vList[i], USER_INPUT_B[i],  xem.HalfCountRealTime())
+        cList.append(testerGui)
+    
+    print "cList: ",  cList
     #testerGui.show()
     
     ### global control for MVC
