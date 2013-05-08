53de7267687e69dd763a024ed322e93a3e6b792b
wonjsohn <wonjsohn@localhost>
2013-05-07 18:39:16 -0700

    [snapshot] pymunk recalibrated

--- projects/balance_limb_pymunk/__main__.py
+++ projects/balance_limb_pymunk/__main__.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-if __name__ == "__main__":
-    import sys, PyQt4
-    sys.path.append('../../../source/py/')
-    from Controls import User
-    app = PyQt4.QtGui.QApplication(sys.argv)
-    user = User()
-    user.show()
-    sys.exit(app.exec_())
+if __name__ == '__main__':
+    import sys
+    sys.path.append('../../source/py/multC_tester')
+    sys.path.append('../../source/py/')
+    from Utilities import *
+    from M_Fpga import SomeFpga # Model in MVC   
+    from config_test import NUM_NEURON, SAMPLING_RATE, FPGA_OUTPUT_B1, FPGA_OUTPUT_B2, FPGA_OUTPUT_B3,   USER_INPUT_B1,  USER_INPUT_B2,  USER_INPUT_B3
+  
+    xem_spindle_tri = SomeFpga(NUM_NEURON, SAMPLING_RATE, '12320003RN')
+    xem_spindle_bic = SomeFpga(NUM_NEURON, SAMPLING_RATE, '124300046A')
+    xem_muscle_bic = SomeFpga(NUM_NEURON, SAMPLING_RATE, '1201000216')
+    xem_muscle_tri = SomeFpga(NUM_NEURON, SAMPLING_RATE, '12430003T2')
--- projects/balance_limb_pymunk/limb_pymunk.py
+++ projects/balance_limb_pymunk/limb_pymunk.py
@@ -4,11 +4,22 @@ from pygame.color import *
 import pymunk
 from pymunk import Vec2d
 import math, sys, random
-from Utilities import *
+
+M_PI = 3.1415926
+JOINT_MIN = -1
+JOINT_MAX = 1
+JOINT_RANGE = JOINT_MAX - JOINT_MIN
 
 def to_pygame(p):
     """Small hack to convert pymunk to pygame coordinates"""
     return int(p.x), int(-p.y+600)
+
+def angle2length(angle):
+    max_length = 1.3
+    length = max_length + ((2.0-max_length)-max_length) / (3.14)* (angle- JOINT_MIN)      # angle in rad 
+    length = 0.3*angle+1     # angle in rad 
+    return length
+
 
 def main():
     pygame.init()
@@ -28,7 +39,7 @@ def main():
     moment = pymunk.moment_for_poly(mass, fp)
 
     # left flipper
-    #forearm_body = pymunk.Body(mass, moment)
+    #forearm_body = pymunk.Body(mass, moment_of_inertia)
     forearm_body = pymunk.Body(mass, 0.0372)
     forearm_body.position = 300, 300
     forearm_shape = pymunk.Poly(forearm_body, [(-x,y) for x,y in fp])
@@ -36,34 +47,47 @@ def main():
 
     elbot_joint_body = pymunk.Body()
     elbot_joint_body.position = forearm_body.position
-    j = pymunk.PinJoint(forearm_body, elbot_joint_body, (0,0), (0,0))
+#    j = pymunk.PinJoint(forearm_body, elbot_joint_body, (0,0), (0,0))
+    j = pymunk.RotaryLimitJoint(forearm_body, elbot_joint_body, JOINT_MIN, JOINT_MAX)
+    
     JOINT_DAMPING_SCHEIDT2007 = 2.1
-    s = pymunk.DampedRotarySpring(forearm_body, elbot_joint_body, -0.0, 0.0, JOINT_DAMPING_SCHEIDT2007)
+    JOINT_DAMPING = JOINT_DAMPING_SCHEIDT2007 * 1
+    s = pymunk.DampedRotarySpring(forearm_body, elbot_joint_body, -0.0, 0.0, JOINT_DAMPING)
     space.add(j, s)
 
     forearm_shape.group = 1
     forearm_shape.elasticity = 0.4
+    
 
+    rest_joint_angle = 0.0
+    
     while running:
         
-        force0 = max(0.0, xem0.ReadFPGA(0x30, "float32")) / 128
-        force1 = max(0.0, xem1.ReadFPGA(0x30, "float32")) / 128
-        forearm_body.torque = (force0-force1) * -0.015
+        # Get forces
+        force_bic = max(0.0, xem_muscle_bic.ReadFPGA(0x22, "float32")) / 128 #- 0.2
+        force_tri = max(0.0, xem_muscle_tri.ReadFPGA(0x22, "float32")) / 128 #- 2.64
+        forearm_body.torque = (force_bic - force_tri) * -1.0
         
         #lce = 1.0
                              
-        angle = ((forearm_body.angle + M_PI) % (2*M_PI)) - M_PI
-        rest_len = 1.01
-        lce0 = angle / (2*M_PI) * 0.5 + rest_len
-        lce1 = 2.0 * rest_len - lce0
+        angle = ((forearm_body.angle + M_PI) % (2*M_PI)) - M_PI - rest_joint_angle
         
-        # Send lce0 &lce1
-        bitVal = ConvertType(lce0, fromType = 'f', toType = 'I')
-        xem0.SendPara(bitVal = bitVal, trigEvent = 8)
-        bitVal = ConvertType(lce1, fromType = 'f', toType = 'I')
-        xem1.SendPara(bitVal = bitVal, trigEvent = 8)
-        print "lce0 = %.2f :: lce1 = %.2f :: total_torque = %.2f" % (lce0, lce1, forearm_body.torque)                            
-        print "force0 = %.2f :: force1 = %.2f" % (force0, force1)                            
+#        lce_bic = angle / (2*M_PI) * 1.5 + rest_len
+#        lce_tri = 2 * rest_len - lce_bic 
+        lce_bic = angle2length(angle)+ 0.02
+        lce_tri = 2.04 - lce_bic;
+        
+        # Send lce of biceps 
+        bitVal = convertType(lce_bic, fromType = 'f', toType = 'I')
+        xem_muscle_bic.SendPara(bitVal = bitVal, trigEvent = 9)
+        xem_spindle_bic.SendPara(bitVal = bitVal, trigEvent = 9)
+        
+        # Send lce of triceps
+        bitVal = convertType(lce_tri, fromType = 'f', toType = 'I')
+        xem_muscle_tri.SendPara(bitVal = bitVal, trigEvent = 9)
+        xem_spindle_tri.SendPara(bitVal = bitVal, trigEvent = 9)
+        print "lce0 = %.2f :: lce1 = %.2f :: total_torque = %.2f" % (lce_bic, lce_tri, forearm_body.torque),                           
+        print "force0 = %.2f :: force1 = %.2f :: angle = %.2f" % (force_bic, force_tri,  angle)                            
 
         
         #r_flipper_body.apply_impulse(Vec2d.unit() * 40000, (force * 20,0))
@@ -74,12 +98,13 @@ def main():
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 running = False
             elif event.type == KEYDOWN and event.key == K_j:
-                forearm_body.torque += 80.0
+                forearm_body.torque -= 5.0
                 pass
             elif event.type == KEYDOWN and event.key == K_f:
                 #forearm_body.apply_force(Vec2d.unit() * -40000, (-100,0))
-                forearm_body.torque -= 80.0
-
+                forearm_body.torque += 5.0
+            elif event.type == KEYDOWN and event.key == K_z:
+                rest_joint_angle = angle 
         ### Clear screen
         screen.fill(THECOLORS["white"])
 
@@ -105,19 +130,25 @@ def main():
 
 if __name__ == '__main__':
     import sys
+    sys.path.append('../../source/py/multC_tester')
     sys.path.append('../../source/py/')
-    from Fpga import Model   
+    from Utilities import *
+    from M_Fpga import SomeFpga # Model in MVC   
+    from config_test import NUM_NEURON, SAMPLING_RATE, FPGA_OUTPUT_B1, FPGA_OUTPUT_B2, FPGA_OUTPUT_B3,   USER_INPUT_B1,  USER_INPUT_B2,  USER_INPUT_B3
+  
+    xem_spindle_tri = SomeFpga(NUM_NEURON, SAMPLING_RATE, '12320003RN')
+    xem_spindle_bic = SomeFpga(NUM_NEURON, SAMPLING_RATE, '124300046A')
+    xem_muscle_bic = SomeFpga(NUM_NEURON, SAMPLING_RATE, '1201000216')
+    xem_muscle_tri = SomeFpga(NUM_NEURON, SAMPLING_RATE, '12430003T2')
     
-    xem0 = Model(board=0)
-    xem1 = Model(board=1)
-
-    value = 10
-    newHalfCnt = 1 * 200 * (10 **6) / SAMPLING_RATE / NUM_NEURON / (value*2) / 2 / 2
-    xem0.SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
-    xem1.SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
     
-    bitVal = ConvertType(20.0, fromType = 'f', toType = 'I')
-    xem0.SendPara(bitVal = bitVal, trigEvent = 1)
-    xem1.SendPara(bitVal = bitVal, trigEvent = 1)
+#    value = 10
+#    newHalfCnt = 1 * 200 * (10 **6) / SAMPLING_RATE / NUM_NEURON / (value*2) / 2 / 2
+#    xem0.SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
+#    xem1.SendPara(bitVal = newHalfCnt, trigEvent = DATA_EVT_CLKRATE)
+#    
+#    bitVal = ConvertType(20.0, fromType = 'f', toType = 'I')
+#    xem0.SendPara(bitVal = bitVal, trigEvent = 1)
+#    xem1.SendPara(bitVal = bitVal, trigEvent = 1)
     
     sys.exit(main())
