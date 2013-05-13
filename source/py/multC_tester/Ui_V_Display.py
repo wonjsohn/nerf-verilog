6d7f4e6599663c289cfbfb2b9b567c9517b130ac
wonjsohn <wonjsohn@localhost>
2013-05-13 12:01:00 -0700

    added two missing files for running python

---
+++ source/py/multC_tester/Ui_V_Display.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '/home/eric/nerf_verilog_eric/source/py/multC_tester/V_Display.ui'
+#
+# Created: Tue Mar 12 16:20:11 2013
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.setWindowModality(QtCore.Qt.WindowModal)
+        Dialog.resize(895, 669)
+        self.comboBox = QtGui.QComboBox(Dialog)
+        self.comboBox.setGeometry(QtCore.QRect(10, 20, 111, 31))
+        self.comboBox.setStyleSheet(_fromUtf8("border-color: rgb(0, 98, 255);"))
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.checkBox = QtGui.QCheckBox(Dialog)
+        self.checkBox.setGeometry(QtCore.QRect(150, 20, 97, 22))
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.listWidget = QtGui.QListWidget(Dialog)
+        self.listWidget.setGeometry(QtCore.QRect(680, 10, 201, 111))
+        self.listWidget.setObjectName(_fromUtf8("listWidget"))
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(750, 120, 66, 17))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.checkBox_2 = QtGui.QCheckBox(Dialog)
+        self.checkBox_2.setGeometry(QtCore.QRect(280, 20, 151, 22))
+        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+        self.comboBox.setItemText(0, QtGui.QApplication.translate("Dialog", "waveform 1", None, QtGui.QApplication.UnicodeUTF8))
+        self.comboBox.setItemText(1, QtGui.QApplication.translate("Dialog", "waveform 2", None, QtGui.QApplication.UnicodeUTF8))
+        self.comboBox.setItemText(2, QtGui.QApplication.translate("Dialog", "waveform 3", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkBox.setText(QtGui.QApplication.translate("Dialog", "auto scale", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Dialog", ".bit files", None, QtGui.QApplication.UnicodeUTF8))
+        self.checkBox_2.setText(QtGui.QApplication.translate("Dialog", "input from wave", None, QtGui.QApplication.UnicodeUTF8))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Dialog = QtGui.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
