ea55a06fd7b8cddd464fde7824ce320111821b46
wonjsohn <wonjsohn@localhost>
2013-05-16 13:25:25 -0700

    two-board setting updated to new spindle

--- source/clk_related/spikecnt.v
+++ source/clk_related/spikecnt.v
@@ -1,3 +1,59 @@
+module spike_counter(
+                        input wire clk,
+                        input wire reset,
+                        input wire spike_in,
+                        output reg [31:0] spike_count
+    );
+
+    reg spike;
+    reg state;
+    reg [6:0] neuron_index;
+    reg [31:0] count;
+    always @ (posedge clk or posedge reset)
+    begin
+        if (reset) begin
+            state <= 1;
+            neuron_index<=0;
+            spike <= 0;
+        end else begin
+            case(state)
+                0:  begin
+                    neuron_index <= neuron_index + 1;
+                    state <= 1;
+                    spike <= spike;
+                    end
+                1:  begin
+                    neuron_index <= neuron_index;
+                    state <= 0;
+                    spike <= spike_in;
+                    end
+             endcase
+        end
+    end
+    
+    wire reset_bar;
+assign reset_bar = ~reset;
+
+always @ (negedge clk or negedge reset_bar) begin
+    if (~reset_bar) begin
+       count <= 0;
+       spike_count <= 0;
+    end else begin
+        if (state) begin
+            count <= count + spike;
+            if (neuron_index == 7'h7f) begin
+                count <= spike;
+                spike_count <= count;
+            end
+        end
+    end
+end
+    
+endmodule
+
+
+
+
 module spikecnt_async(spike, int_cnt_out, fast_clk, slow_clk, reset, clear_out, cnt, sig1, sig2, read);
     input   spike, slow_clk, fast_clk, reset;
     output  reg [31:0] int_cnt_out, cnt;
