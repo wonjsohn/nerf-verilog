f96045973519b1827508c625ac2a790dd6dbba7f
wonjsohn <wonjsohn@gmail.com>
2016-03-13 01:48:06 -0500

    Revert "updated CN board. ~30ms cortical neuron delay."
    This reverts commit 981ac71a4965e28bf7a8dabf8e227947aa97e177.

--- source/iz_cortialneuron.v
+++ source/iz_cortialneuron.v
@@ -143,7 +143,7 @@ module iz_corticalneuron_th_control(
 //    wire [31:0] spike_history2_mem; //extra delay for positive phase 09/09/14
 //    wire [31:0] spike_history2_mem_in; //extra delay for positive phase
 
-    assign spike_history_mem_in = first_pass ? 0 : {spike_history_mem[30:0], fired};
+//    assign spike_history_mem_in = first_pass ? 0 : {spike_history_mem[30:0], fired};
 //    assign spike_history2_mem_in = first_pass ? 0 : {spike_history2_mem_in[30:0], spike_history_mem[31]}; //concatenate 2 mems of size 32. 
     
     reg state;
@@ -192,14 +192,14 @@ always @ (negedge clk or negedge reset_bar) begin
     end else begin
         if (state) begin
             //each_spike <= fired;
-            each_spike <= spike_history_mem[26];  // was 14 (32ms)
+            each_spike <= spike_history_mem[28];  // was 14 (32ms)
             //each_spike <= spike_history2_mem[28];  //  (64ms)
             history[neuron_index] <= fired;
             if (neuron_index == 7'h7f) begin
                 first_pass <= 0;
                 v_out <= v_mem_in;
                 //spike <= fired;
-                spike <= spike_history_mem[26];  // was 14 (32ms)
+                spike <= spike_history_mem[28];  // was 14 (32ms)
                 //spike <= spike_history2_mem[28];  //  (64ms)
                 //population <= {fired, history[126:0]};
                 population <= history;
